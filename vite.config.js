9ca7c534036af0ef58cfdb236a5dd480d9cbf898
subhan <subhan2208e@aptechgdn.net>
2025-10-22 06:27:55 +0500

    prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
+
   return {
-    base: `/${env.VITE_REPO_NAME}/`,
+    base: `/${env.VITE_REPO_NAME}/`,  // 👈 GitHub Pages repo name
     plugins: [react(), tailwindcss()],
     server: {
       open: true,
-      host: false, // Allow access from network devices
+      host: false,
       proxy: {
         "/api": {
           target: "http://localhost:5000",
