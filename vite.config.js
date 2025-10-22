7620e8f92b6517284afa02b80f9dd2633a9ce8bb
subhan <subhan2208e@aptechgdn.net>
2025-10-22 06:17:10 +0500

    fixed base path

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,15 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
-
   return {
+    base: `/${env.VITE_REPO_NAME}/`,
     plugins: [react(), tailwindcss()],
-    base: "/personalPortfolio/", 
     server: {
       open: true,
-      host: false,
+      host: false, // Allow access from network devices
       proxy: {
         "/api": {
           target: "http://localhost:5000",
