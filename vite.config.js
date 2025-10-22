129921a285ba162be73ace389c067a0f78f72669
subhan <subhan2208e@aptechgdn.net>
2025-10-22 07:23:50 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/personalPortfolio/",  // 👈 GitHub Pages repo name directly
-  plugins: [react(), tailwindcss()],
-  server: {
-    open: true,
-    host: false,
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        secure: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    base: `/${env.VITE_REPO_NAME}/`,  // 👈 GitHub Pages repo name
+    plugins: [react(), tailwindcss()],
+    server: {
+      open: true,
+      host: false,
+      proxy: {
+        "/api": {
+          target: "http://localhost:5000",
+          changeOrigin: true,
+          secure: true,
+        },
       },
     },
-  },
+  };
 });
