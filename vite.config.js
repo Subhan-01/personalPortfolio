8c885c33d24bafdb5834a2a9d6f83ae809fd02e0
subhan <subhan2208e@aptechgdn.net>
2025-10-22 07:20:35 +0500

    fix: correct base path

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,20 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return {
-    base: `/${env.VITE_REPO_NAME}/`,  // 👈 GitHub Pages repo name
-    plugins: [react(), tailwindcss()],
-    server: {
-      open: true,
-      host: false,
-      proxy: {
-        "/api": {
-          target: "http://localhost:5000",
-          changeOrigin: true,
-          secure: true,
-        },
+export default defineConfig({
+  base: "/personalPortfolio/",  // 👈 GitHub Pages repo name directly
+  plugins: [react(), tailwindcss()],
+  server: {
+    open: true,
+    host: false,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: true,
       },
     },
-  };
+  },
 });
